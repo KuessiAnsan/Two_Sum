9bba40b743a1960296286dd5d7e777bc849efc06
KuessiAnsan <kuessiansandjiyehoue@gmail.com>
2022-08-11 11:24:42 +0000

    commit1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int target;
+        char choice;
+        int [] numbers = {2, 3, 6, 8, 7, 9, 1, 10, 15, 19};
+        System.out.println("Let me show you a magic trick : )\n");
+
+        do {
+            System.out.println("Enter the target: ");
+            target = scn.nextInt();
+            System.out.println(Arrays.toString(numbers));
+            for (int i = 0; i < numbers.length; i++) {
+                for (int j = 0; j < numbers.length; j++) {
+                    if (numbers[i] + numbers[j] == target) {
+                        System.out.println("The indices are: " + i + ", " + j);
+                    } /*else {
+                        System.out.println("No indices");
+                    }*/
+                }
+            }
+            System.out.println("Voulez vous continuer? (Y/y)");
+            choice = scn.next().charAt(0);
+        }while (choice == 'Y' || choice == 'y');
+    }
+}
